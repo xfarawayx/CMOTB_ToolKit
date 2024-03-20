5ccf9f7820de1414ba0ba1bf83188ca3f01e302d
xfarawayx <58210164+xfarawayx@users.noreply.github.com>
2024-03-20 15:43:33 +0800

    upload toolkit

---
+++ Evaluation_Tools/Hard/utils/config_tracker.m
@@ -0,0 +1,10 @@
+function trackers = config_tracker()
+% config trackers to be evaluated (for RGBNIR)
+
+
+trackers = {    
+    struct('name', 'MAFNet_DiMP', 'publish', '1111')...
+    struct('name', 'MAFNet_RT', 'publish', '1111')...  
+};
+
+end
---
+++ Evaluation_Tools/Joint/utils/config_sequence.m
@@ -0,0 +1,24 @@
+function sequences = config_sequence()
+% config sequences for evaluation
+% the configuration files are placed in ./sequence_evaluation_config/;
+
+%dataset_name = 'all_dataset.txt';
+dataset_name = 'VC.txt';
+%dataset_name = 'sw1.txt';
+
+% check if the file exists
+if ~exist(dataset_name, 'file')
+    error('%s is not found!', dataset_name);
+end
+
+% load evaluation sequences
+fid = fopen(dataset_name, 'r');
+i = 0;
+sequences = cell(100000, 1);
+while ~feof(fid)
+    i = i + 1;
+    sequences{i, 1} = fgetl(fid);
+end
+sequences(i+1:end) = [];
+fclose(fid);
+end
